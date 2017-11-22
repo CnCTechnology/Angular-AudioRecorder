d6a6484bcc448ef2953a4ac21723dc77c64eaff0
shekhar kumar <bulletinbible@gmail.com>
2017-11-22 18:19:04 +0530

    Initial commit

---
+++ Source/index.js
@@ -0,0 +1,17 @@
+﻿var app = angular.module('audioRecorderApp', ['ui.router','audioRecorder']);
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+
+   
+
+    var homeState = {
+        name: 'home',
+        url: '/',
+        templateUrl: '../views/home.html',
+        controller: 'HomeCtrl'
+    }    
+    $stateProvider.state(homeState);
+ 
+
+});
---
+++ Source/views/home.js
@@ -0,0 +1,8 @@
+﻿app.controller("HomeCtrl", ['$scope', 'recorderInitializerFactory', function ($scope, recorderInitializerFactory) {
+    $scope.name = "Home Controller";
+    recorderInitializerFactory.InitAudio();
+    $scope.toggleRecording = function (ev) {
+        recorderInitializerFactory.ToggleRecording(ev.target);
+    }
+}
+]);
